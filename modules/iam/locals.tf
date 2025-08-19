edcd6f4c703937b911348dff708eeb25349f9243
Lokesh C <challagulla.l@northeastern.edu>
2025-08-18 23:45:32 -0400

    fix

--- modules/iam/locals.tf
+++ modules/iam/locals.tf
@@ -7,7 +7,10 @@ locals {
   service_accounts = flatten([
     for app_name, app in local.apps_config :
     [
-      for sa in try(app.service_accounts, []) : merge(sa, { app = app_name })
+      for sa in try(app.service_accounts, []) : merge(sa, {
+        app                = app_name
+        service_account_id = length(regexall(".*@.*", try(sa.account_id, ""))) > 0 ? try(sa.account_id, "") : "${try(sa.account_id, "")}@${var.project_id}.iam.gserviceaccount.com"
+      })
     ]
   ])
 
--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -20,6 +20,14 @@ resource "google_service_account" "service-account" {
   project      = var.project_id
 }
 
+resource "google_service_account_iam_member" "workload_identity_binding" {
+  for_each           = { for service_account in local.service_accounts : "${service_account.app}:${service_account.account_id}" => service_account }
+  service_account_id = "projects/${var.project_id}/serviceAccounts/${each.value.service_account_id}"
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "serviceAccount:${var.project_id}.svc.id.goog[${each.value.ns}/${each.value.kubernetes_service_account}]"
+  depends_on         = [google_service_account.service-account]
+}
+
 resource "google_storage_bucket_iam_member" "storage-bucket-iam-member" {
   for_each = { for storage_bucket in local.storage_buckets : "${storage_bucket.app}:${storage_bucket.name}" => storage_bucket }
 
